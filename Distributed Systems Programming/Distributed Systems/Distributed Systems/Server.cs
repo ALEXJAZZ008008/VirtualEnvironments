6bd3a73cdbfbb97c55d9fc673cc9e34d0993a34d
483446 <483446@7700f37f-d485-3e40-9b60-5bcdc4074596>
2017-02-09 14:05:25 +0000

    Distributed systems lab 1

---
+++ Distributed Systems Programming/Distributed Systems/Distributed Systems/Server.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Server
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the local IP address (or just press <ENTER> to listen at 127.0.0.1): ");
+            string address = Console.ReadLine();
+
+            if (address == "")
+            {
+                address = "127.0.0.1";
+            }
+
+            Console.Write("Please enter the port (or just press <ENTER> to listen at 5000): ");
+            string portString = Console.ReadLine();
+            int port = 5000;
+
+            if (portString != "")
+            {
+                try
+                {
+                    port = int.Parse(portString);
+                }
+                catch
+                {
+                    Console.WriteLine("Invalid entry, port set to 5000.");
+                }
+            }
+
+            TcpListener tcpListener = new TcpListener(IPAddress.Parse(address), port);
+            tcpListener.Start();
+
+            Console.WriteLine("Listening at port " + port + "...");
+
+            while (true)
+            {
+                TcpClient tcpClient = tcpListener.AcceptTcpClient();
+                NetworkStream nStream = tcpClient.GetStream();
+
+                byte[] header = new byte[6];
+
+                nStream.Read(header, 0, 6);
+
+                int messageType = header[3];
+                int remainingLength = header[4] * 256 + header[5];
+
+                Console.WriteLine("Message type: " + messageType);
+                Console.WriteLine("Length: " + remainingLength);
+
+                byte[] rawData = new byte[remainingLength];
+
+                nStream.Read(rawData, 0, remainingLength);
+                nStream.Flush();
+                nStream.Close();
+
+                tcpClient.Close();
+
+                int offset = rawData[0];
+                int dataLength = rawData[1] * 256 + rawData[2];
+
+                byte[] byteMessage = new byte[remainingLength];
+                Array.Copy(rawData, 3, byteMessage, 0, dataLength);
+                string Message = System.Text.Encoding.ASCII.GetString(rawData, 3, dataLength);
+
+                Console.WriteLine("Encryption key: " + offset);
+                Console.WriteLine("Data length: " + dataLength);
+                Console.WriteLine("Received message:");
+                Console.WriteLine(Message);
+
+                for (int i = 0; i < Message.Length; i++)
+                {
+                    byteMessage[i] = (byte)((int)byteMessage[i] + (int)offset);
+
+                    if (byteMessage[i] > 126)
+                    {
+                        byteMessage[i] = (byte)((int)byteMessage[i] - 126 + 31);
+                    }
+                }
+
+                string EncryptedMessage = System.Text.Encoding.ASCII.GetString(byteMessage, 0, dataLength);
+
+                Console.WriteLine("Encrypted message:");
+                Console.WriteLine(EncryptedMessage);
+            }
+        }
+    }
+}
